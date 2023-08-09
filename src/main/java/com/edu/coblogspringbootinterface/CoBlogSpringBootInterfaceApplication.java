ce561fdb2e98efc779523ec350bb22a0bb07b328
15506 <15506531157@163.com>
2023-08-09 12:35:20 +0800

    Undefined6608

---
+++ src/main/java/com/edu/coblogspringbootinterface/CoBlogSpringBootInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.edu.coblogspringbootinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoBlogSpringBootInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoBlogSpringBootInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/coblogspringbootinterface/CoBlogSpringBootInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.coblogspringbootinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoBlogSpringBootInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
